a637d1f582f8f32913267826b6ed85c96c39358d
Brad <bmoran620@gmail.com>
2021-09-11 15:06:01 -0400

    Try different form

--- src/pages/MarioKarty.js
+++ src/pages/MarioKarty.js
@@ -13,99 +13,13 @@ export function MarioKarty(props) {
             <div className="image-container main-image">
                 <img className="img-fluid center" src="marioKarty64.png" alt="alternative" width="50%" />
             </div>
-            <form name="contact" netlify>
-                <InputGroup className="mb-3 mario-kart-radio">
-                    <div className="radio">
-                        <label>
-                            <input
-                                type="radio"
-                                value="Mario"
-                                // checked={this.state.selectedOption === "Male"}
-                                // onChange={this.onValueChange}
-                            />
-                            Mario
-                        </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input
-                                type="radio"
-                                value="Luigi"
-                                // checked={this.state.selectedOption === "Male"}
-                                // onChange={this.onValueChange}
-                            />
-                            Luigi
-                        </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input
-                                type="radio"
-                                value="Peach"
-                                // checked={this.state.selectedOption === "Male"}
-                                // onChange={this.onValueChange}
-                            />
-                            Peach
-                        </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input
-                                type="radio"
-                                value="Toad"
-                                // checked={this.state.selectedOption === "Male"}
-                                // onChange={this.onValueChange}
-                            />
-                            Toad
-                        </label>
-                    </div>
-                </InputGroup>
-                <InputGroup className="mb-3 mario-kart-radio">
-                    <div className="radio">
-                        <label>
-                            <input
-                                type="radio"
-                                value="Yoshi"
-                                // checked={this.state.selectedOption === "Male"}
-                                // onChange={this.onValueChange}
-                            />
-                            Yoshi
-                        </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input
-                                type="radio"
-                                value="D.K."
-                                // checked={this.state.selectedOption === "Male"}
-                                // onChange={this.onValueChange}
-                            />
-                            D.K.
-                        </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input
-                                type="radio"
-                                value="Wario"
-                                // checked={this.state.selectedOption === "Male"}
-                                // onChange={this.onValueChange}
-                            />
-                            Wario
-                        </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input
-                                type="radio"
-                                value="Bowser"
-                                // checked={this.state.selectedOption === "Male"}
-                                // onChange={this.onValueChange}
-                            />
-                            Bowser
-                        </label>
-                    </div>
-                </InputGroup>
+            <form name="character" netlify>
+                <input type="radio" id="mario" name="mario" value="MARIO"/>
+                <label className="radio" for="mario">MARIO</label>
+                <input type="radio" id="luigi" name="luigi" value="LUIGI"/>
+                <label className="radio" for="luigi">LUIGI</label>
+                <input type="radio" id="peach" name="peach" value="PEACH"/>
+                <label className="radio" for="peach">PEACH</label>
                 <div className="mario-kart-submit">
                     <Button variant="primary" type="submit">Submit</Button>
                 </div>
