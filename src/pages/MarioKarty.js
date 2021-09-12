2f73c85720a2d0f2bd8ac9977022480b0b974fab
Brad <bmoran620@gmail.com>
2021-09-12 11:40:05 -0400

    Add additional characters and name field

--- src/pages/MarioKarty.js
+++ src/pages/MarioKarty.js
@@ -16,13 +16,67 @@ export function MarioKarty(props) {
             <form name="character" method="post">
                 <input type="hidden" name="form-name" value="character" />
                 <p>
-                    <label className="radio">Mario <input type="radio" name="mario"/></label>
+                    <label className="name-input">Your Name: <input type="text" name="name"/></label>
                 </p>
                 <p>
-                    <label className="radio">Luigi <input type="radio" name="luigi"/></label>
+                    <label className="radio">Mario <input type="radio" name="char" value="mario"/></label>
                 </p>
                 <p>
-                    <label className="radio">Peach <input type="radio" name="peach"/></label>
+                    <label className="radio">Luigi <input type="radio" name="char" value="luigi"/></label>
+                </p>
+                <p>
+                    <label className="radio">Peach <input type="radio" name="char" value="peach"/></label>
+                </p>
+                <p>
+                    <label className="radio">Wario <input type="radio" name="char" value="wario"/></label>
+                </p>
+                <p>
+                    <label className="radio">Waluigi <input type="radio" name="char" value="waluigi"/></label>
+                </p>
+                <p>
+                    <label className="radio">Yoshi <input type="radio" name="char" value="yoshi"/></label>
+                </p>
+                <p>
+                    <label className="radio">D.K. <input type="radio" name="char" value="D.K>"/></label>
+                </p>
+                <p>
+                    <label className="radio">Toad <input type="radio" name="char" value="toad"/></label>
+                </p>
+                <p>
+                    <label className="radio">Bowser <input type="radio" name="char" value="bowser"/></label>
+                </p>
+                <p>
+                    <label className="radio">Daisy <input type="radio" name="char" value="daisy"/></label>
+                </p>
+                <p>
+                    <label className="radio">Boo <input type="radio" name="char" value="boo"/></label>
+                </p>
+                <p>
+                    <label className="radio">Diddy Kong <input type="radio" name="char" value="diddy-kong"/></label>
+                </p>
+                <p>
+                    <label className="radio">Koopa Troopa <input type="radio" name="char" value="koopa-troopa"/></label>
+                </p>
+                <p>
+                    <label className="radio">Goomba <input type="radio" name="char" value="goomba"/></label>
+                </p>
+                <p>
+                    <label className="radio">Birdo <input type="radio" name="char" value="birdo"/></label>
+                </p>
+                <p>
+                    <label className="radio">Chain Chomp <input type="radio" name="char" value="chain-chomp"/></label>
+                </p>
+                <p>
+                    <label className="radio">Rosalina <input type="radio" name="char" value="rosalina"/></label>
+                </p>
+                <p>
+                    <label className="radio">Baby Bowser <input type="radio" name="char" value="baby-bowser"/></label>
+                </p>
+                <p>
+                    <label className="radio">Pirhana Plant <input type="radio" name="char" value="pirhana-plant"/></label>
+                </p>
+                <p>
+                    <label className="radio">Toadette <input type="radio" name="char" value="toadette"/></label>
                 </p>
                 <p>
                     <button type="submit">Send</button>
