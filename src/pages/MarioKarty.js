4875b4e9d0e1c09fc2404aa7d7026dd4d0824537
Brad <bmoran620@gmail.com>
2021-09-12 12:13:00 -0400

    Clean up

--- src/pages/MarioKarty.js
+++ src/pages/MarioKarty.js
@@ -15,72 +15,84 @@ export function MarioKarty(props) {
             </div>
             <form name="character" method="post">
                 <input type="hidden" name="form-name" value="character" />
+                <div class="name-input">
                 <p>
                     <label className="name-input">Your Name: <input type="text" name="name"/></label>
                 </p>
+                </div>
+                <div class="col-12 mario-kart-radio">
                 <p>
-                    <label className="radio">Mario <input type="radio" name="char" value="Mario"/></label>
+                    <input type="radio" name="char" value="Mario"/><label className="radio">Mario</label>
                 </p>
                 <p>
-                    <label className="radio">Luigi <input type="radio" name="char" value="Luigi"/></label>
+                    <input type="radio" name="char" value="Luigi"/><label className="radio">Luigi</label>
                 </p>
                 <p>
-                    <label className="radio">Peach <input type="radio" name="char" value="Peach"/></label>
+                    <input type="radio" name="char" value="Peach"/><label className="radio">Peach</label>
                 </p>
                 <p>
-                    <label className="radio">Wario <input type="radio" name="char" value="Wario"/></label>
+                    <input type="radio" name="char" value="Wario"/><label className="radio">Wario</label>
                 </p>
                 <p>
-                    <label className="radio">Waluigi <input type="radio" name="char" value="Waluigi"/></label>
+                    <input type="radio" name="char" value="Waluigi"/><label className="radio">Waluigi</label>
+                </p>
+                </div>
+                <div class="col-12 mario-kart-radio">
+                <p>
+                    <input type="radio" name="char" value="Yoshi"/><label className="radio">Yoshi</label>
                 </p>
                 <p>
-                    <label className="radio">Yoshi <input type="radio" name="char" value="Yoshi"/></label>
+                    <input type="radio" name="char" value="DK"/><label className="radio">D.K.</label>
                 </p>
                 <p>
-                    <label className="radio">D.K. <input type="radio" name="char" value="DK"/></label>
+                    <input type="radio" name="char" value="Toad"/><label className="radio">Toad</label>
                 </p>
                 <p>
-                    <label className="radio">Toad <input type="radio" name="char" value="Toad"/></label>
+                    <input type="radio" name="char" value="Bowser"/><label className="radio">Bowser</label>
                 </p>
                 <p>
-                    <label className="radio">Bowser <input type="radio" name="char" value="Bowser"/></label>
+                    <input type="radio" name="char" value="Daisy"/><label className="radio">Daisy</label>
+                </p>
+                </div>
+                <div class="col-12 mario-kart-radio">
+                <p>
+                    <input type="radio" name="char" value="Boo"/><label className="radio">Boo</label>
                 </p>
                 <p>
-                    <label className="radio">Daisy <input type="radio" name="char" value="Daisy"/></label>
+                    <input type="radio" name="char" value="Diddy-Kong"/><label className="radio">Diddy Kong</label>
                 </p>
                 <p>
-                    <label className="radio">Boo <input type="radio" name="char" value="Boo"/></label>
+                    <input type="radio" name="char" value="Koopa-Troopa"/><label className="radio">Koopa Troopa</label>
                 </p>
                 <p>
-                    <label className="radio">Diddy Kong <input type="radio" name="char" value="Diddy-Kong"/></label>
+                    <input type="radio" name="char" value="Goomba"/><label className="radio">Goomba</label>
                 </p>
                 <p>
-                    <label className="radio">Koopa Troopa <input type="radio" name="char" value="Koopa-Troopa"/></label>
+                    <input type="radio" name="char" value="Birdo"/><label className="radio">Birdo</label>
+                </p>
+                </div>
+                <div class="col-12 mario-kart-radio">
+                <p>
+                    <input type="radio" name="char" value="Chain-Chomp"/><label className="radio">Chain Chomp</label>
                 </p>
                 <p>
-                    <label className="radio">Goomba <input type="radio" name="char" value="Goomba"/></label>
+                    <input type="radio" name="char" value="Rosalina"/><label className="radio">Rosalina</label>
                 </p>
                 <p>
-                    <label className="radio">Birdo <input type="radio" name="char" value="Birdo"/></label>
+                    <input type="radio" name="char" value="Baby-Bowser"/><label className="radio">Baby Bowser</label>
                 </p>
                 <p>
-                    <label className="radio">Chain Chomp <input type="radio" name="char" value="Chain-Chomp"/></label>
+                    <input type="radio" name="char" value="Pirhana-Plant"/><label className="radio">Pirhana Plant</label>
                 </p>
                 <p>
-                    <label className="radio">Rosalina <input type="radio" name="char" value="Rosalina"/></label>
+                    <input type="radio" name="char" value="Toadette"/><label className="radio">Toadette</label>
                 </p>
-                <p>
-                    <label className="radio">Baby Bowser <input type="radio" name="char" value="Baby-Bowser"/></label>
-                </p>
-                <p>
-                    <label className="radio">Pirhana Plant <input type="radio" name="char" value="Pirhana-Plant"/></label>
-                </p>
-                <p>
-                    <label className="radio">Toadette <input type="radio" name="char" value="Toadette"/></label>
-                </p>
+                </div>
+                <div class="mario-kart-submit">
                 <p>
                     <button type="submit">Send</button>
                 </p>
+                </div>
             </form>
         </div>
         </Fragment>
